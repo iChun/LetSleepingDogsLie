934fe5526b24d5a65aabbf5af79d2b328fc7c77c
iChun <ohai.iChun@gmail.com>
2019-10-04 05:57:54 +0800

    Sign the jar.

--- src/main/java/me/ichun/letsleepingdogslie/common/LetSleepingDogsLie.java
+++ src/main/java/me/ichun/letsleepingdogslie/common/LetSleepingDogsLie.java
@@ -12,16 +12,19 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 @Mod(modid = LetSleepingDogsLie.MODID, name = LetSleepingDogsLie.NAME,
         version = LetSleepingDogsLie.VERSION,
+        certificateFingerprint = LetSleepingDogsLie.CERT_FINGERPRINT,
         clientSideOnly = true,
         acceptableRemoteVersions = "*",
         dependencies = "required-after:forge@[13.19.0.2141,)",
@@ -33,6 +36,8 @@ public class LetSleepingDogsLie
     public static final String NAME = "LetSleepingDogsLie";
 
     public static final String VERSION = "1.0.1";
+
+    public static final String CERT_FINGERPRINT = "4db5c2bd1b556f252a5b8b54b256d381b2a0a6b8";
 
     @Mod.Instance(MODID)
     public static LetSleepingDogsLie instance;
@@ -98,6 +103,15 @@ public class LetSleepingDogsLie
         else
         {
             init();
+        }
+    }
+
+    @Mod.EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if(event.getSource() != null && event.getSource().isFile())
+        {
+            logger.warn("The file " + event.getSource().getName() + " has been modified. Support for the mod will not be provided.");
         }
     }
 
