b9cefd92f181e969c2c313ee80725948e0cd462f
iChun <ohai.iChun@gmail.com>
2024-06-26 15:52:38 +0100

    Backport 1.19.3 to 1.19.2. Use of iChunUtil's Gradle workspace for this, lots of files moved around/removed.

---
+++ common/src/main/java/me/ichun/mods/dogslie/common/core/Config.java
@@ -0,0 +1,73 @@
+package me.ichun.mods.dogslie.common.core;
+
+import com.google.common.collect.ImmutableList;
+import me.ichun.mods.dogslie.common.LetSleepingDogsLie;
+import me.ichun.mods.ichunutil.common.config.ConfigBase;
+import me.ichun.mods.ichunutil.common.config.annotations.Prop;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config extends ConfigBase
+{
+    public static final List<String> DEFAULT_POSES = ImmutableList.of(
+        "forelegStraight",
+        "forelegSprawled",
+        "forelegSprawledBack",
+        "forelegSkewed",
+        "forelegSide",
+        "hindlegStraight",
+        "hindlegStraightBack",
+        "hindlegSprawled",
+        "hindlegSprawledBack",
+        "hindlegSide"
+    );
+
+    public boolean dogsSpawnLying = true;
+
+    @Prop(min = 1, max = 6000000)
+    public int timeBeforeLie = 15 * 20;
+
+    @Prop(min = 0D, max = 32D)
+    public double rangeBeforeGettingUp = 3D;
+
+    public LetSleepingDogsLie.GetsUpFor getsUpTo = LetSleepingDogsLie.GetsUpFor.OWNER;
+
+    @Prop(validator = "validateEnabledPoses")
+    public List<String> enabledPoses = new ArrayList<>(DEFAULT_POSES);
+
+    public Config()
+    {
+        super("letsleepingdogslie.toml");
+    }
+
+    public boolean validateEnabledPoses(Object o)
+    {
+        if(o instanceof String s)
+        {
+            return DEFAULT_POSES.contains(s);
+        }
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public String getModId()
+    {
+        return LetSleepingDogsLie.MOD_ID;
+    }
+
+    @NotNull
+    @Override
+    public String getConfigName()
+    {
+        return LetSleepingDogsLie.MOD_NAME;
+    }
+
+    @Override
+    public Type getConfigType()
+    {
+        return Type.CLIENT;
+    }
+}
---
+++ forge/src/main/java/me/ichun/mods/dogslie/loader/forge/LoaderForge.java
@@ -0,0 +1,41 @@
+package me.ichun.mods.dogslie.loader.forge;
+
+import me.ichun.mods.dogslie.common.LetSleepingDogsLie;
+import me.ichun.mods.dogslie.common.core.Config;
+import me.ichun.mods.ichunutil.client.gui.config.WorkspaceConfigs;
+import me.ichun.mods.ichunutil.common.iChunUtil;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(LetSleepingDogsLie.MOD_ID)
+public class LoaderForge extends LetSleepingDogsLie
+{
+    public LoaderForge()
+    {
+        modProxy = this;
+
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> this::initClient);
+        DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.error("You are loading " + MOD_NAME + " on a server. " + MOD_NAME + " is a client only mod!"));
+
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (remoteVersion, isFromServer) -> true));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private void initClient()
+    {
+        //register config
+        config = iChunUtil.d().registerConfig(new Config());
+
+        MinecraftForge.EVENT_BUS.register(eventHandlerClient = new EventHandlerClientForge());
+
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new WorkspaceConfigs(screen)));
+    }
+}
